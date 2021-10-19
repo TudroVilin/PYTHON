1477cf894b095f58753404aff175ed5dcab25792
TudroVilin <tudelavillafuerte@gmail.com>
2021-10-19 19:29:25 +0200

    commit

---
+++ dianaciminto.py
@@ -0,0 +1,34 @@
+from math import trunc
+print("Ejercicio Día nacimiento")
+print("Introduzca el día de su nacimiento:")
+d=int(input())
+print("Introduzca el mes de su nacimiento:")
+m=int(input())
+print("Introduzca el año de su nacimiento:")
+a=int(input())
+if(m==1):
+    m=13
+    a=a-1
+elif(m==2):
+    m=14
+    a=a+1
+d2=trunc((d+(m*2)+a+((m+1)*3/5)+(a/4)-(a/100)+(a/400)+2))
+d3=trunc(d2/7)
+d1=d2-(d3*7)
+if(d1==1):
+    print("Domingo")
+elif(d1==2):
+    print("Lunes")
+elif(d1==3):
+    print("Martes")
+elif(d1==4):
+    print("Miércoles")
+elif(d1==5):
+    print("Jueves")
+elif(d1==6):
+    print("Viernes")
+elif(d1==0):
+    print("Sábado")
+else:
+    print("ERROR")
+print("Fin programa")
---
+++ libreriamath.py
@@ -0,0 +1,14 @@
+#Normalmente, los import suelen ir al
+# principio de un programa, al igual que
+# las variables
+from math import floor, ceil, trunc
+print("Librería math Python")
+n1=20
+n2=3
+d=n1/n2
+print("La división es: "+str(d))
+#Vamos a utilizar el método floor:
+# Redondea al inferior
+print("Floor: "+str(floor(d)))
+print("Ceil: "+str(ceil(d)))
+print("Trunc: "+str(trunc(d)))
